3b6f474ebf20b5bb1012b8dafb4e6dded656d645
github username <github email address>
2019-09-09 15:49:18 -0400

    Update login routing logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email_address: params[:email_address])
         if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect_to breweries_path
+        redirect_to user_path(user)
         else 
             render :new
         end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params)
     session[:user_id] = user.id
-    redirect_to breweries_path
+    redirect_to user_path(user)
   end 
 
   def update
