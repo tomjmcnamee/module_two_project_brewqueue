db0e25a1d41b22dd7b5fb0ed7793cb48b5e46569
github username <github email address>
2019-09-09 14:33:59 -0400

    Changes to login and brewery search

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
-    
+
     def new
     end 
 
     def create
         user = User.find_by(email_address: params[:email_address])
         if user && user.authenticate(params[:password])
-        session[:user_id]
+        session[:user_id] = user.id
         redirect_to breweries_path
         else 
             render :new
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -6,24 +6,20 @@ class Brewery < ApplicationRecord
     validates :name, presence: true
     
     def self.search_for_brewery(name, city, state)
-        result_objs_arr = []
-        Brewery.all.each do |brewery|
-          if brewery.name.downcase.include?(name) && brewery.city.downcase.include?(city) && brewery.state == state
+      result_objs_arr = []
+      Brewery.all.each do |brewery|
+        if state == ""
+          if brewery.name.downcase.include?(name) && brewery.city.downcase.include?(city)
             result_objs_arr << brewery
           end # Ends IF string
-        end  # ends student.all.each loop
-        # result_objs_arr.each do |brewery|
-        #     if brewery.city.downcase.include?(city)
-        #         result_objs_arr << brewery
-        #         end # Ends IF string
-        #     end  # ends student.all.each loop
-        # result_objs_arr.each do |brewery|
-        #     if brewery.where(state: state)
-        #         result_objs_arr << brewery
-        #         end # Ends IF string
-        #     end  # ends student.all.each loop
-        result_objs_arr
-    end
+        else
+          if brewery.name.downcase.include?(name) && brewery.city.downcase.include?(city) && brewery.state == state
+            result_objs_arr << brewery
+          end # ends IF string
+        end  # ends IF Statement about a "" state
+      end  # ends brewery.all.each loop
+      result_objs_arr
+    end  
 
     def self.list_all_valid_states
         Brewery.select(:state).map { |rec| rec.state }.uniq
