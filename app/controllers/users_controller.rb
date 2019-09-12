7109f17bc52ba5c2316d5fc4779957d4d161dff7
github username <github email address>
2019-09-12 10:05:59 -0400

    Update to Auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
+    @user.save
     redirect_to users_path(@user)
   end 
 
